10758aa321c5a83e1d73f23ae8245abb58d27d1b
alexlinus <alexlinus93@yandex.com>
2019-01-03 15:11:27 +0600

    added autocomplete for select field

--- calculate/forms.py
+++ calculate/forms.py
@@ -3,7 +3,8 @@ from django.forms import ModelForm
 from django import forms
 
 class CarModelForm(ModelForm):
-    model_title = forms.ChoiceField(choices=[(model.id, model.model_title) for model in CarModel.objects.all()]) #здесь я короче говорю,, что вместо инпута должен быть select, и в choices (значения селекта) я вставляю title всех объектов модели. Зачем ID добавил к тайтул, потому что так требует choices, два аргумента, в частности айди. ID он записывает в VALUE. Получается потом мы можем брать это value и брать выбранный объект по id. Круто!
+    my_custom_title = 'Выберите модель'
+    model_title = forms.ChoiceField(choices=[(model.id, model.model_title) for model in CarModel.objects.all()], widget=forms.Select(attrs={'value': 'Выберите модель', 'classs': 'demo-default'})) #здесь я короче говорю,, что вместо инпута должен быть select, и в choices (значения селекта) я вставляю title всех объектов модели. Зачем ID добавил к тайтул, потому что так требует choices, два аргумента, в частности айди. ID он записывает в VALUE. Получается потом мы можем брать это value и брать выбранный объект по id. Круто!
     class Meta:
         model = CarModel
         fields = ['model_title']
